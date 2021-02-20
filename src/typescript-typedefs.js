b7fdcddf7fec642bf7f46eb8378b1cb9ea28f345
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-19 20:11:24 -0800

    Add comment

--- src/typescript-typedefs.js
+++ src/typescript-typedefs.js
@@ -1,5 +1,6 @@
 const { printSchemaWithDirectives } = require("@graphql-tools/utils");
 
+// https://github.com/dotansimha/graphql-code-generator/issues/3899
 const print = (schema) => `
   export const typeDefs = gql\`${schema}\`;
 `;
