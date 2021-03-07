91defdb8d9dcf01edbb1227f9dee534f5a799657
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-03-07 11:26:39 -0800

    Run Prettier

--- src/typescript-typedefs.ts
+++ src/typescript-typedefs.ts
@@ -1,4 +1,4 @@
-import {printSchemaWithDirectives} from "@graphql-tools/utils";
+import { printSchemaWithDirectives } from "@graphql-tools/utils";
 import { GraphQLSchema, stripIgnoredCharacters } from "graphql";
 
 // https://github.com/dotansimha/graphql-code-generator/issues/3899
@@ -7,4 +7,5 @@ const print = (schema: string) => `
   export const typeDefs = gql\`${schema}\`;
 `;
 
-export const plugin = (schema: GraphQLSchema) => print(stripIgnoredCharacters(printSchemaWithDirectives(schema)))
+export const plugin = (schema: GraphQLSchema) =>
+  print(stripIgnoredCharacters(printSchemaWithDirectives(schema)));
